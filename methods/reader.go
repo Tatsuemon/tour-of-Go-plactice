0ab1ab6377293a1cde1079e6bf3517b20beae5e2
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-05-30 23:19:52 +0900

    add reader & image

---
+++ methods/reader.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main(){
+	r := strings.NewReader("Hello, Reader!")
+
+	b := make([]byte, 8)
+
+	/*
+		func (T) Read(b []byte) (n int, err error)
+		データを与えられたバイトスライスへいれ、入れたバイトのサイズとエラーの値を返す
+		ストリームの終端は io.EOF
+	*/
+
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
+	}
+}
